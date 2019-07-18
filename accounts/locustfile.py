3675912c1dd5312fedd3d739032d156d4c8003ab
Michael Harrison <mwharrison@rice.edu>
2019-07-18 15:11:50 -0500

    catch post response to callback

--- accounts/locustfile.py
+++ accounts/locustfile.py
@@ -1,9 +1,16 @@
 from locust import HttpLocust, TaskSet
 
-def lookup(l):
-    l.client.post("/authenticate", {"login[username_or_email]":"admin"})
+from bs4 import BeautifulSoup
 
-    # with l.client.get("/", catch_response=True) as response:
+def lookup(l):
+    with l.client.post("/auth/identity/callback", {"login[username_or_email]":"admin",
+                                              "authenticity_token": "cJ+lhaRuDnRCq+N4zbudQ5TGwRzkvIvA5wYvuQMUD3jFHoC9D5bQx44Ak4glSrjWmST4ioJNCxwq1gMNYPOfqg==",
+                                              "utf8": "✓",
+                                              "login[source]": "authenticate",
+                                              "commit": "Log+in"}, catch_response=True) as response:
+        print(response.content)
+
+    # with l.client.get("/profile", catch_response=True) as response:
     #     print(response.content)
 
 # def logout(l):
@@ -16,8 +23,27 @@ def lookup(l):
 # def profile(l):
 #     l.client.get("/profile")
 
-class UserBehavior(TaskSet):
-    #tasks = {index: 2, profile: 1}
+class AccountsTasks(TaskSet):
+
+    # def on_start(self):
+    #     """
+    #     on_start is called when a Locust start before,
+    #     any task is scheduled
+    #     """
+    #     self.login()
+    #
+    # def login(self):
+    #     resp = self.client.get("/login")
+    #     parsed_html = BeautifulSoup(resp.content)
+    #     form_build_id = parsed_html.body.find('input', {'name': 'form_build_id'})['value']
+    #
+    #     self.client.post("/lookup_login", {
+    #         "login[username_or_email]": "admin",
+    #         "form_id": "user_login_form",
+    #         "authenticity_token": "cJ+lhaRuDnRCq+N4zbudQ5TGwRzkvIvA5wYvuQMUD3jFHoC9D5bQx44Ak4glSrjWmST4ioJNCxwq1gMNYPOfqg==",
+    #         "form_build_id": form_build_id,
+    #         "op": "Log in"
+    #     })
 
     def on_start(self):
         lookup(self)
@@ -26,6 +52,6 @@ class UserBehavior(TaskSet):
     #     logout(self)
 
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+    task_set = AccountsTasks
     min_wait = 5000
     max_wait = 9000
