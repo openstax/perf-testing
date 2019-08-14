fecef1da48bc2de69ef427827d164830d5fbbe66
Michael Harrison <mwharrison@rice.edu>
2019-08-14 09:34:54 -0500

    update accounts load tests

--- accounts/locustfile.py
+++ accounts/locustfile.py
@@ -1,52 +1,71 @@
-from realbrowserlocusts import FirefoxLocust, ChromeLocust, PhantomJSLocust
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from locust import TaskSet, task
+import logging
 
-platform_url = "https://accounts-dev.openstax.org/"
-login_username = "admin"
-login_password = ""
+from lxml import html
+from locust import HttpLocust, TaskSet
+from slimit.parser import Parser
 
-class LocustUserBehavior(TaskSet):
+jsparser = Parser()
+logger = logging.getLogger(__name__)
 
-    # Functions to Run on Start and End
+
+def init_security_tokens(loc):
+    res = loc.client.get('/')
+    res_html = html.fromstring(res.text)
+    csrf_token = res_html.xpath("//meta[@name='csrf-token']/@content")[0]
+    csrf_param = res_html.xpath("//meta[@name='csrf-param']/@content")[0]
+    loc.csrf_token = csrf_token
+    loc.csrf_param = csrf_param
+    loc.client.headers['X-CSRF-Token'] = csrf_token
+    loc.client.headers['X-Requested-With'] = 'XMLHttpRequest'
+    loc.client.headers['User-Agent'] = 'Chrome/999.999.99 locust/1.0'
+
+
+def login_user(loc):
+    users = {} #fill in users here (until we have a csv), like so: {'username':'password',}
+    if not (hasattr(loc, 'csrf_param')):
+        init_security_tokens(loc)
+
+    res = loc.client.get('/')
+
+    for username, password in users.items():
+        res_html = html.fromstring(res.text)
+        login_url = res_html.xpath('//form')[0].action
+        data = {i.name:i.value for i in res_html.xpath("//form//input")}
+
+        data['login[username_or_email]'] = username
+
+        res2 = loc.client.post(login_url, data=data)
+        res2_html = html.fromstring(res2.text)
+        login2_url = res2_html.xpath('//form')[0].action
+
+        data2 = {i.name: i.value for i in res2_html.xpath("//form//input")}
+        data2['login[password]'] = password
+        res3 = loc.client.post(login2_url, data=data2)
+
+        res4 = loc.client.get('/api/user')
+        print(res4.json())
+
+def user_api(loc):
+    loc.client.get('/api/user')
+
+
+def index(loc):
+    loc.client.get('/')
+
+class UserBehavior(TaskSet):
+    tasks = {index: 1, user_api: 5 }
+
     def on_start(self):
-        self.client.timed_event_for_locust("Load", "Login Page", self.load_login_page)
-        self.client.timed_event_for_locust("Enter", "Username", self.enter_login_username)
-        self.client.timed_event_for_locust("Enter", "Password", self.enter_login_password)
+        login_user(self)
 
-    # Helper Functions
-    def load_login_page(self):
-        self.client.get(platform_url+'login')
-        self.client.wait.until(EC.visibility_of_element_located((By.ID, 'login_username_or_email')), "Login field is visible.")
+    def on_stop(self):
+        self.client.get('/accounts/logout')
 
-    def enter_login_username(self):
-        login_field = self.client.find_element_by_id('login_username_or_email')
-        login_field.send_keys(login_username)
 
-        self.client.find_element_by_name('commit').click()
-
-        self.client.wait.until(EC.visibility_of_element_located((By.ID, 'login_password')), "Password field is visible.")
-
-    def enter_login_password(self):
-        login_field = self.client.find_element_by_id('login_password')
-        login_field.send_keys(login_password)
-
-        self.client.find_element_by_name('commit').click()
-
-        self.client.wait.until(EC.visibility_of_element_located((By.ID, 'application-body')), "Application body is visible.")
-
-    # Main Tasks        
-    @task(2)
-    def query_api(self):
-        self.client.get(platform_url+'api/user')
-
-class LocustUser(ChromeLocust):
-    host = "not really used"
-    timeout = 30 #in seconds in waitUntil thingies
-    min_wait = 100
-    max_wait = 1000
-    screen_width = 1200
-    screen_height = 600
-    task_set = LocustUserBehavior
+class GeneralUser(HttpLocust):
+    task_set = UserBehavior
+    stop_timeout = 1200
+    weight = 9
