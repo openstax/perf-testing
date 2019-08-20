122b684940dc76caea53a85d3da868b09394f5e7
Ross Reedstrom <reedstrm@gmail.com>
2019-08-20 16:50:34 -0500

    formatting, correct naming convention for class, extend usage comment

--- mylocust.py
+++ mylocust.py
@@ -1,13 +1,15 @@
 from locust import HttpLocust
 
 
-class mylocust(HttpLocust):
+class MyLocust(HttpLocust):
     """
     Helper class to use when debugging locust tasks.
 
-    Instantiate with host paramers, and call task functions
-    with instance as first argument
+    Instantiate with host parameter, then use it as parent to instantiate
+    a locust.TaskSet instance, then call task functions with that instance
+    as first argument
     """
-    def __init__(self, host='http://localhost'):
+
+    def __init__(self, host="http://localhost"):
         self.host = host
         super().__init__()
