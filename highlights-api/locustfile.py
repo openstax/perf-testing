bffeffe95482f8790296be291328e08efd3c1c96
Steve Burkett <steve@burkettnet.com>
2019-12-12 15:38:22 -0800

    Change to add to dictionary if notes, prev HL, next HL is present

--- highlights-api/locustfile.py
+++ highlights-api/locustfile.py
@@ -23,7 +23,6 @@ COLORS = ['yellow', 'green', 'blue', 'purple', 'pink']
 
 logger = logging.getLogger(__name__)
 
-
 class ApiBehavior(TaskSet):
   def on_start(self):
     self.user_uuid = random.choice(USERS)
@@ -40,7 +39,7 @@ class ApiBehavior(TaskSet):
     length = random.randrange(10, 5000)
     return ''.join(choice(ascii_uppercase) for i in range(length))
 
-  def post_highlight(self, id, source_id, prev_highlight_id, next_highlight_id, annotation):
+  def post_highlight(self, id, source_id, prev_highlight_id, next_highlight_id, randomly_add_note):
     logger.info('post_highlight. User {}, id {}, source_id {}, prev_highlight_id {}, scope_id {}'.format(self.user_uuid, id, source_id, prev_highlight_id, self.scope_id))
 
     post_params = {
@@ -50,14 +49,21 @@ class ApiBehavior(TaskSet):
         "source_id": source_id,
         "anchor": "id301",
         "highlighted_content": "red cow",
-        "annotation": self.random_string() if annotation else null,
-        "prev_highlight_id": prev_highlight_id,
-        "next_highlight_id": next_highlight_id,
         "color": random.choice(COLORS),
         "scope_id": self.scope_id,
         "location_strategies": [{"type":"TextPositionSelector","start":"12","end":"10"}]
       }
     }
+
+    if randomly_add_note:
+      post_params['highlight']['annotation'] = self.random_string()
+
+    if prev_highlight_id != None:
+      post_params['highlight']['prev_highlight_id'] = prev_highlight_id
+
+    if next_highlight_id != None:
+      post_params['highlight']['next_highlight_id'] = next_highlight_id
+
     res = self.client.post("/api/v0/highlights", json=post_params)
     logger.info("post_note {}".format(res))
 
@@ -66,16 +72,16 @@ class ApiBehavior(TaskSet):
     self.client.headers['loadtest_client_uuid'] = self.user_uuid
 
     source_id = random.choice(self.source_ids.keys())
-    colors = random.sample(COLORS, random.randint(1, len(COLORS)))
-    res = self.client.get("/api/v0/highlights?source_ids={}&source_type=openstax_page&color={}".format(source_id, ','.join(colors)))
+    color = random.choice(COLORS)
+    res = self.client.get("/api/v0/highlights?source_ids={}&source_type=openstax_page&color={}".format(source_id, color))
     logger.info("get {}".format(res))
 
   @task(4)
   def get_summary(self):
     self.client.headers['loadtest_client_uuid'] = self.user_uuid
 
-    colors = random.sample(COLORS, random.randint(1, len(COLORS)))
-    res = self.client.get("/api/v0/highlights/summary?source_type=openstax_page&color={}".format(','.join(colors)))
+    color = random.choice(COLORS)
+    res = self.client.get("/api/v0/highlights/summary?source_type=openstax_page&color={}".format(color))
     logger.info("get_summary {}".format(res))
 
   # 2 highlights within one page (source), one book (scope) per add task
@@ -83,14 +89,22 @@ class ApiBehavior(TaskSet):
   def add_highlight(self):
     self.client.headers['loadtest_client_uuid'] = self.user_uuid
     source_id = random.choice(self.source_ids.keys())
-    highlights = self.source_ids[source_id];
+    highlight_ids = self.source_ids[source_id]
 
-    i = random.randint(0, len(highlights) - 1) if len(highlights) > 0 else 0 
-   
-    prev_highlight = highlights[i-1]
-    next_highlight = highlights[i]
-      
-    self.post_first(id, source_id, prev_highlight, next_highlight, random.choice([1,2,3,4]) < 2)
+    new_hl_id = str(uuid.uuid4())
+    num_hls = len(highlight_ids)
+    if num_hls == 0:
+      self.post_highlight(new_hl_id, source_id, None, None, random.choice([1,2,3,4]) < 2)
+      self.source_ids[source_id].append(new_hl_id)
+    elif num_hls == 1:
+      self.post_highlight(new_hl_id, source_id, highlight_ids[0], None, random.choice([1,2,3,4]) < 2)
+      self.source_ids[source_id].append(new_hl_id)
+    else:
+      i = random.randint(1, len(highlight_ids)-1)
+      prev_highlight_id = highlight_ids[i-1]
+      next_highlight_id = highlight_ids[i]
+      self.post_highlight(new_hl_id, source_id, prev_highlight_id, next_highlight_id, random.choice([1,2,3,4]) < 2)
+      self.source_ids[source_id].insert(i, new_hl_id)
 
 class HighlightsApiTest(HttpLocust):
   task_set = ApiBehavior
